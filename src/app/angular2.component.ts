fecbdd3b97475237a01cc1e99d528240f42f5d81
Yo <yoelnacho@gmail.com>
2016-06-14 20:18:55 -0300

    First Steps

---
+++ src/app/angular2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'angular2-app',
+  templateUrl: 'angular2.component.html',
+  styleUrls: ['angular2.component.css']
+})
+export class Angular2AppComponent {
+  title = 'angular2 works!';
+}
